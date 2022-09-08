e80a6c9151a50b87543aa5703c456f2826f46f9d
Peter Butkovic <butkovic@gmail.com>
2022-09-08 13:16:31 +0200

    refactor: switch to python based impl

---
+++ print_versions.py
@@ -0,0 +1,68 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import re
+import sys
+import getopt
+from packaging.version import Version
+
+def get_releases(url):
+    page = requests.get(url, headers={'User-Agent': 'Mozilla/5'})
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.findAll("a", href=re.compile('^([.0-9]*)\/$'))
+    releases = []
+
+    for result in results:
+        releases.append(result['href'].replace('/', ''))
+    return releases
+
+def get_pushed_tags(url):
+    response = requests.get(url, headers={'User-Agent': 'Mozilla/5'})
+
+    tags = []
+    if (response.status_code != 404):
+        tags = []
+        json = response.json()
+        print(json)
+        tags = [container['name'] for container in json['results']]
+
+    return tags
+
+def main(argv):
+    latestOnly = False
+
+    try:
+        opts, args = getopt.getopt(argv, "hlt:r:", ["tags-url=", "releases-url="])
+    except getopt.GetoptError:
+        print
+        'print_versions.py -t <dockerhub tags url> -r <releases url> [--latest-only]'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print
+            'print_versions.py -t <dockerhub tags url> -r <release url> [--latest-only]'
+            sys.exit()
+        elif opt in ("-t", "--tags-url"):
+            urlTags = arg
+        elif opt in ("-r", "--releases-url"):
+            urlReleases = arg
+        elif opt in ("-l", "--latest-only"):
+            latestOnly = True
+
+    releases = get_releases(urlReleases)
+    tags = get_pushed_tags(urlTags)
+
+    result = [x for x in releases if x not in tags]
+    result.sort(key=Version)
+
+    if not result:
+        print(json.dumps(""))
+    else:
+        if latestOnly:
+            print(json.dumps(result[-1]))
+        else:
+            print(json.dumps(result))
+    sys.exit()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
